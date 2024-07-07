9ea2c35afa7a28e01c7e595382de26245ea4cecd
Guy Sandler <guy@sandler.co.il>
2024-07-07 12:26:17 -0700

    logo + navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+    import Navbar from "./navbar.svelte";
+</script>
+<div>
+    <nav>
+        <Navbar />
+    </nav>
+    <slot />
+</div>
---
+++ src/routes/navbar.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, DarkMode } from 'flowbite-svelte';
+</script>
+<div>
+    <Navbar let:NavContainer>
+        <NavContainer class="border px-5 py-2 rounded-lg bg-white dark:bg-gray-600">
+            <NavBrand href="/">
+                <img src="src/images/printerbench.svg" class="me-6 h-12 sm:h-15" alt="PrinterBench Logo" />
+                <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">PrinterBench</span>
+            </NavBrand>
+            <div class="flex md:order-2">
+                <Button size="sm">Get started</Button>
+                <DarkMode />
+                <NavHamburger />
+            </div>
+            <NavUl class="order-1">
+                <NavLi href="/leaderboard">LeaderBoard</NavLi>
+                <NavLi href="/add">Add Printer</NavLi>
+                <NavLi href="/profile">My Profile</NavLi>
+            </NavUl>
+        </NavContainer>
+    </Navbar>
+</div>
