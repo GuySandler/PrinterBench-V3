c3aa9cb444a7aea47fc4edb698feadc495897679
Guy Sandler <guy@sandler.co.il>
2024-07-07 22:42:55 -0700

    form 2

--- src/routes/add/+page.svelte
+++ src/routes/add/+page.svelte
@@ -1,8 +1,45 @@
 <script lang="ts">
-  import { Label, Input, Button, Select, Textarea, Toggle } from "flowbite-svelte";
-  const handleSubmit = () => {
-    alert("Form submited.");
-  };
+    import { Label, Input, Button, Select, Toggle, Range, P } from "flowbite-svelte";
+    import StarRating from 'svelte-star-rating';
+    const handleSubmit = () => {
+        alert("Form submited.");
+    };
+    let rating = 2.5;
+    let name = "";
+    let brand = "";
+    let type = "";
+    let link = "";
+    let speed = "";
+    let acceleration = "";
+    let sizex = "";
+    let sizey = "";
+    let sizez = "";
+    let autoZOffset = false;
+    let autoBedLeveling = false;
+    let powerLossRecovery = false;
+    let filamentRunOutSensor = false;
+    let airPurifier = false;
+    let inputShaping = false;
+    let camera = false;
+    let wifi = false;
+    let remoteAccess = false;
+    let touchscreen = false;
+
+    let config = {
+        readOnly: false,
+        countStars: 7,
+        range: {min: 0, max: 7},
+        score: rating,
+        showScore: true,
+        scoreFormat: function(){ return `(${this.score.toFixed(0)}/${this.countStars})` },
+        starConfig: {
+            size: 500,
+            fillColor: '#FFFFFF',
+            strokeColor: "#FFFFFF",
+            unfilledColor: '#FFFFFF',
+            strokeUnfilledColor: '#FFFFFF'
+        }
+    }
 </script>
 <style>
   .spacer {
@@ -12,58 +49,72 @@
 <div class="w-9/12">
     <form>
         <Label for="name">Printer Name</Label>
-        <Input autocomplete="autocomplete_off_randString"  id="name" placeholder="Ender 3 V3 SE" />
+        <Input bind:value={name} autocomplete="autocomplete_off_randString"  id="name" placeholder="Ender 3 V3 SE" />
 
         <Label for="brand">Printer Brand</Label>
-        <Input autocomplete="autocomplete_off_randString"  id="brand" placeholder="Prusa" />
+        <Input bind:value={brand} autocomplete="autocomplete_off_randString"  id="brand" placeholder="Prusa" />
 
         <Label for="type">Type</Label>
-        <Select id="type">
+        <Select bind:value={type} id="type">
             <option value=bedslinger>Bedslinger</option>
             <option value=corexy>CoreXY</option>
             <option value=delta>Delta</option>
             <!-- <option value=resin>resin</option> -->
         </Select>
 
+        <Label for="link">Printer Brand</Label>
+        <Input bind:value={link} autocomplete="autocomplete_off_randString"  id="link" placeholder="https://www.prusa3d.com/product/original-prusa-mk4/" />
+
         <Label for="brand">Speed (according to offical website in mm/s)</Label>
-        <Input autocomplete="autocomplete_off_randString"  id="brand" placeholder="600" />
+        <Input bind:value={speed} autocomplete="autocomplete_off_randString"  id="speed" placeholder="600" />
         <Label for="brand">Acceleration (according to offical website in mm/s)</Label>
-        <Input autocomplete="autocomplete_off_randString"  id="brand" placeholder="20000" />
+        <Input bind:value={acceleration} autocomplete="autocomplete_off_randString"  id="brand" placeholder="20000" />
 
-        <Label for="type">Build Size X (cm)</Label>
-        <Input autocomplete="autocomplete_off_randString"  id="size" placeholder="Enter printer build size X (cm)" />
-        <Label for="type">Build Size Y (cm)</Label>
-        <Input autocomplete="autocomplete_off_randString"  id="size" placeholder="Enter printer build size Y (cm)" />
-        <Label for="type">Build Size Z (cm)</Label>
-        <Input autocomplete="autocomplete_off_randString"  id="size" placeholder="Enter printer build size Z (cm)" />
-
+        <div style="display:inline-block">
+            <Label for="type">Build Size X (cm)</Label>
+            <Input bind:value={sizex} style="width:10vw" autocomplete="autocomplete_off_randString"  id="size" placeholder="Enter printer build size X (cm)" />
+        </div>
+        <div style="display:inline-block">
+            <Label for="type">Build Size Y (cm)</Label>
+            <Input bind:value={sizey} style="width:10vw" autocomplete="autocomplete_off_randString"  id="size" placeholder="Enter printer build size Y (cm)" />
+        </div>
+        <div style="display:inline-block">
+            <Label for="type">Build Size Z (cm)</Label>
+            <Input bind:value={sizez} style="width:10vw" autocomplete="autocomplete_off_randString"  id="size" placeholder="Enter printer build size Z (cm)" />
+        </div>
         <Label for="features">Features</Label>
         <div style="display:inline-block;border-right:1px solid gray;padding-right:15px">
-            <Toggle>Auto Z offset</Toggle>
+            <Toggle bind:checked={autoZOffset}>Auto Z offset</Toggle>
             <div class="spacer" />
-            <Toggle>Auto Bed Leveling</Toggle>
+            <Toggle bind:checked={autoBedLeveling}>Auto Bed Leveling</Toggle>
             <div class="spacer" />
-            <Toggle>Power Loss Recovery</Toggle>
+            <Toggle bind:checked={powerLossRecovery}>Power Loss Recovery</Toggle>
             <div class="spacer" />
-            <Toggle>Filament Run-out Sensor</Toggle>
+            <Toggle bind:checked={filamentRunOutSensor}>Filament Run-out Sensor</Toggle>
         </div>
         <div style="display:inline-block;border-right:1px solid gray;margin-left:15px;padding-right:15px">
-            <Toggle>Air Purifier</Toggle>
+            <Toggle bind:checked={airPurifier}>Air Purifier</Toggle>
             <div class="spacer" />
-            <Toggle>Input Shaping</Toggle>
+            <Toggle bind:checked={inputShaping}>Input Shaping</Toggle>
             <div class="spacer" />
-            <Toggle>Camera</Toggle>
+            <Toggle bind:checked={camera}>Camera</Toggle>
         </div>
         <div style="display:inline-block;margin-left:15px;">
-            <Toggle>Wifi</Toggle>
+            <Toggle bind:checked={wifi}>Wifi</Toggle>
             <div class="spacer" />
-            <Toggle>Remote Access</Toggle>
+            <Toggle bind:checked={remoteAccess}>Remote Access</Toggle>
             <div class="spacer" />
-            <Toggle>Touchscreen</Toggle>
+            <Toggle bind:checked={touchscreen}>Touchscreen</Toggle>
         </div>
 
-        <Label for="description">Description</Label>
-        <Textarea id="description" placeholder="Enter printer description" />
+        <Label for="brand">Overall Rating (0.00 - 5.00)</Label>
+        <div style="display:inline-block">
+            <StarRating config={config} rating={rating} />
+        </div>
+        <Range bind:value={rating} style="width:20vw;margin-right:10px;padding-right:10px" max={5} step={0.25} id="rating" />
+        <P size="3xl" style="display:inline-flex;width:5vw">{rating}</P>
+
+        <br>
         <Button type="submit" on:click={handleSubmit}>Submit</Button>
     </form>
 </div>
