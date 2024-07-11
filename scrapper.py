5ac4f0d6cea7f3c9da99228a766d430642b4a9df
Guy Sandler <guy@sandler.co.il>
2024-07-11 15:32:04 -0700

    scrapper 3

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
+import cloudscraper
+
+crealityLinks = [
+    "https://store.creality.com/products/ender-3-v3-plus-3d-printer",
+    "https://store.creality.com/products/ender-3-v3-corexz-3d-printer",
+    "https://store.creality.com/products/ender-3-v3-se-3d-printer",
+    "https://store.creality.com/products/ender-3-v3-ke-3d-printer",
+    "https://store.creality.com/products/ender-3-s1-3d-printer-aa7d",
+    "https://store.creality.com/products/ender-3-s1-plus-3d-printer",
+    "https://store.creality.com/products/ender-3-s1-pro-3d-printer",
+    "https://store.creality.com/products/ender-3-v2-neo-3d-printer",
+    "https://store.creality.com/products/ender-3-max-neo-3d-printer",
+    "https://store.creality.com/products/ender-3-v2-3d-printer",
+    "https://store.creality.com/products/ender-3-neo-3d-printer",
+    "https://store.creality.com/products/cr-10-se-3d-printer",
+    "https://store.creality.com/products/cr-m4-3d-printer",
+    "https://store.creality.com/products/cr-10-smart-pro-3d-printer",
+    "https://store.creality.com/products/cr-30-3d-printer",
+    "https://store.creality.com/products/cr-6-max-3d-printer",
+    "https://store.creality.com/products/ender-5-s1-3d-printer",
+    "https://store.creality.com/products/ender-5-plus-3d-printer",
+    "https://store.creality.com/products/k1-max-3d-printer",
+    "https://store.creality.com/products/k1c-3d-printer",
+    "https://store.creality.com/products/k1-3d-printer",
+    "https://store.creality.com/products/sermoon-d3-3d-printer",
+    "https://store.creality.com/products/sermoon-d3-pro-3d-printer"
+]
+bambuLinks = [
+    "https://store.bambulab.com/products/x1-carbon?variant=42059894095987",
+    "https://store.bambulab.com/products/p1s?variant=40877026541683",
+    "https://store.bambulab.com/products/p1p",
+    "https://store.bambulab.com/products/a1?variant=41157296881779",
+    "https://store.bambulab.com/products/a1-mini?variant=40963298558067"
+]
+sovolLinks = [
+    "https://www.sovol3d.com/collections/3d-printer/products/sovol-sv08-3d-printer",
+    "https://www.sovol3d.com/collections/3d-printer/products/sovol-sv06-plus-fully-open-source-3d-printer-with-linear-rail-structure",
+    "https://www.sovol3d.com/collections/3d-printer/products/comgrow-t500-direct-drive-auto-leveling-3d-printer-500-500-500",
+    "https://www.sovol3d.com/collections/3d-printer/products/sovol-sv06-best-budget-3d-printer-for-beginner",
+    "https://www.sovol3d.com/collections/3d-printer/products/comgrow-t300-3d-printer",
+    "https://www.sovol3d.com/collections/3d-printer/products/sovol-sv07-plus-large-klipper-3d-printer",
+    "https://www.sovol3d.com/collections/3d-printer/products/sovol-sv07-klipper-direct-drive-3d-printer-print-speed-250mm-s",
+    "https://www.sovol3d.com/collections/3d-printer/products/sv04"
+]
 
 def elegoo(): #this is a test, might use later when I add resin printers
     # URL = "https://us.elegoo.com/products/mars-5-ultra-9k-7inch-monochrome-lcd-resin-3d-printer"
@@ -32,14 +76,14 @@ def prusa(): #Not scrappable (for now) due to random classes and id for each pro
     print("price: " + cleanPrice)
     print("name: " + name)
 
-def Creality(): #Not scrappable but I think workarounds exist
-    # after researching I found out this might be a cloudflare thing
-    URL = "https://store.creality.com/products/ender-3-v3-plus-3d-printer"
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
-    print(soup.prettify())
+def Creality(URL): #done
+    # URL = "https://store.creality.com/products/ender-3-v3-plus-3d-printer?official-website-index-buy=&spm=..index.home_card_2_2024_1.1"
+    scraper = cloudscraper.create_scraper()
+    page = scraper.get(URL)
+    soup = BeautifulSoup(page.text, "html.parser")
+    # print(soup.prettify())
     results = soup.find("div", class_="product-detail")
-    print(results)
+    # print(results)
     price = results.find("span", class_="money").text
     name = results.find("h1", class_="product-info__header_title").text
     cleanPrice = []
@@ -48,9 +92,10 @@ def Creality(): #Not scrappable but I think workarounds exist
     cleanPrice = ''.join(cleanPrice)
     print("price: " + cleanPrice)
     print("name: " + name)
+    print("-"*10)
 
-def BambuLab(): #works
-    URL = "https://store.bambulab.com/collections/3d-printer/products/p1p"
+def BambuLab(URL): #works
+    # URL = "https://store.bambulab.com/collections/3d-printer/products/p1p"
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
     # print(soup.prettify())
@@ -64,3 +109,28 @@ def BambuLab(): #works
     cleanPrice = ''.join(cleanPrice)
     print("price: " + cleanPrice)
     print("name: " + name)
+    print("-" * 10)
+
+def Sovol(URL): #works
+    # URL = "https://www.sovol3d.com/collections/3d-printer/products/sovol-sv06-plus-fully-open-source-3d-printer-with-linear-rail-structure"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    # print(soup.prettify())
+    results = soup.find("div", class_="product-grid-container")
+    # print(results)
+    price = results.find("span", class_="ht-money").text
+    name = results.find("h1", class_="product-title").text
+    cleanPrice = []
+    for char in price:
+        if char.isdigit() or char == ".": cleanPrice.append(char)
+    cleanPrice = ''.join(cleanPrice)
+    print("price: " + cleanPrice)
+    print("name: " + name)
+    print("-" * 10)
+
+for printer in crealityLinks:
+    Creality(printer)
+for printer in bambuLinks:
+    BambuLab(printer)
+for printer in sovolLinks:
+    Sovol(printer)
